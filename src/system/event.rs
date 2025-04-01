7241c7c52869aa1bb934875d65ad9eaf5fe41a41
undefined <yuhuison@live.com>
2025-04-02 00:16:30 +0800

    u

--- src/system/event.rs
+++ src/system/event.rs
@@ -419,14 +419,6 @@ impl Event {
             sys::EVREventType_VREvent_ButtonUnpress => ButtonUnpress(get(data)),
             sys::EVREventType_VREvent_ButtonTouch => ButtonTouch(get(data)),
             sys::EVREventType_VREvent_ButtonUntouch => ButtonUntouch(get(data)),
-            sys::EVREventType_VREvent_DualAnalog_Press => DualAnalog_Press,
-            sys::EVREventType_VREvent_DualAnalog_Unpress => DualAnalog_Unpress,
-            sys::EVREventType_VREvent_DualAnalog_Touch => DualAnalog_Touch,
-            sys::EVREventType_VREvent_DualAnalog_Untouch => DualAnalog_Untouch,
-            sys::EVREventType_VREvent_DualAnalog_Move => DualAnalog_Move,
-            sys::EVREventType_VREvent_DualAnalog_ModeSwitch1 => DualAnalog_ModeSwitch1,
-            sys::EVREventType_VREvent_DualAnalog_ModeSwitch2 => DualAnalog_ModeSwitch2,
-            sys::EVREventType_VREvent_DualAnalog_Cancel => DualAnalog_Cancel,
             sys::EVREventType_VREvent_MouseMove => MouseMove(get(data)),
             sys::EVREventType_VREvent_MouseButtonDown => MouseButtonDown(get(data)),
             sys::EVREventType_VREvent_MouseButtonUp => MouseButtonUp(get(data)),
@@ -439,14 +431,8 @@ impl Event {
             sys::EVREventType_VREvent_ScrollSmooth => ScrollSmooth(get(data)),
             sys::EVREventType_VREvent_InputFocusCaptured => InputFocusCaptured(get(data)),
             sys::EVREventType_VREvent_InputFocusReleased => InputFocusReleased(get(data)),
-            sys::EVREventType_VREvent_SceneFocusLost => SceneFocusLost(get(data)),
-            sys::EVREventType_VREvent_SceneFocusGained => SceneFocusGained(get(data)),
             sys::EVREventType_VREvent_SceneApplicationChanged => SceneApplicationChanged(get(data)),
-            sys::EVREventType_VREvent_SceneFocusChanged => SceneFocusChanged(get(data)),
             sys::EVREventType_VREvent_InputFocusChanged => InputFocusChanged(get(data)),
-            sys::EVREventType_VREvent_SceneApplicationSecondaryRenderingStarted => {
-                SceneApplicationSecondaryRenderingStarted(get(data))
-            }
             sys::EVREventType_VREvent_SceneApplicationUsingWrongGraphicsAdapter => {
                 SceneApplicationUsingWrongGraphicsAdapter
             }
@@ -461,7 +447,6 @@ impl Event {
             sys::EVREventType_VREvent_DashboardDeactivated => DashboardDeactivated,
             sys::EVREventType_VREvent_DashboardRequested => DashboardRequested,
             sys::EVREventType_VREvent_ResetDashboard => ResetDashboard,
-            sys::EVREventType_VREvent_RenderToast => RenderToast,
             sys::EVREventType_VREvent_ImageLoaded => ImageLoaded,
             sys::EVREventType_VREvent_ShowKeyboard => ShowKeyboard,
             sys::EVREventType_VREvent_HideKeyboard => HideKeyboard,
@@ -494,7 +479,6 @@ impl Event {
             sys::EVREventType_VREvent_Notification_Destroyed => Notification_Destroyed,
             sys::EVREventType_VREvent_Quit => Quit(get(data)),
             sys::EVREventType_VREvent_ProcessQuit => ProcessQuit(get(data)),
-            sys::EVREventType_VREvent_QuitAborted_UserPrompt => QuitAborted_UserPrompt(get(data)),
             sys::EVREventType_VREvent_QuitAcknowledged => QuitAcknowledged(get(data)),
             sys::EVREventType_VREvent_DriverRequestedQuit => DriverRequestedQuit,
             sys::EVREventType_VREvent_RestartRequested => RestartRequested,
@@ -560,27 +544,12 @@ impl Event {
             sys::EVREventType_VREvent_KeyboardClosed => KeyboardClosed,
             sys::EVREventType_VREvent_KeyboardCharInput => KeyboardCharInput(get(data)),
             sys::EVREventType_VREvent_KeyboardDone => KeyboardDone,
-            sys::EVREventType_VREvent_ApplicationTransitionStarted => ApplicationTransitionStarted,
-            sys::EVREventType_VREvent_ApplicationTransitionAborted => ApplicationTransitionAborted,
-            sys::EVREventType_VREvent_ApplicationTransitionNewAppStarted => {
-                ApplicationTransitionNewAppStarted
-            }
             sys::EVREventType_VREvent_ApplicationListUpdated => ApplicationListUpdated,
             sys::EVREventType_VREvent_ApplicationMimeTypeLoad => ApplicationMimeTypeLoad,
-            sys::EVREventType_VREvent_ApplicationTransitionNewAppLaunchComplete => {
-                ApplicationTransitionNewAppLaunchComplete
-            }
             sys::EVREventType_VREvent_ProcessConnected => ProcessConnected,
             sys::EVREventType_VREvent_ProcessDisconnected => ProcessDisconnected,
-            sys::EVREventType_VREvent_Compositor_MirrorWindowShown => Compositor_MirrorWindowShown,
-            sys::EVREventType_VREvent_Compositor_MirrorWindowHidden => {
-                Compositor_MirrorWindowHidden
-            }
             sys::EVREventType_VREvent_Compositor_ChaperoneBoundsShown => {
                 Compositor_ChaperoneBoundsShown
-            }
-            sys::EVREventType_VREvent_Compositor_ChaperoneBoundsHidden => {
-                Compositor_ChaperoneBoundsHidden
             }
             sys::EVREventType_VREvent_Compositor_DisplayDisconnected => {
                 Compositor_DisplayDisconnected
